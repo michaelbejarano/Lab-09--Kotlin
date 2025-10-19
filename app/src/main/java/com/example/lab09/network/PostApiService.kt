c7761bf6a3a7f9f649a1c30bbfe137cd83a224fe
michael <michaelbejarano940@gmail.com>
2025-10-19 06:54:46 -0500

    Crear la interfaz api

---
+++ app/src/main/java/com/example/lab09/network/PostApiService.kt
@@ -0,0 +1,14 @@
+package com.example.lab09.network
+
+import com.example.lab09.model.PostModel
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface PostApiService {
+    @GET("posts")
+    suspend fun getUserPosts(): List<PostModel>
+
+    @GET("posts/{id}")
+    suspend fun getUserPostById(@Path("id") id: Int): PostModel
+
+}
